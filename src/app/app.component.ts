2b9c439de4b554296ee13799f482f66bef87d180
ataerg <ataerg@github.com>
2025-04-13 21:35:04 +0200

    Added several users and created component to select user by random ID

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppHeaderComponent],
+  imports: [RouterOutlet, UserComponent, AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
